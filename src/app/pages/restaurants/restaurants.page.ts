caeff265bcab82a25cf2e039444aa001753ca890
Ioan Narcis <Ioannarcis14@gmail.com>
2022-05-03 16:23:42 +0200

    created repo and desgined some demo pages

---
+++ src/app/pages/restaurants/restaurants.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants',
+  templateUrl: './restaurants.page.html',
+  styleUrls: ['./restaurants.page.scss'],
+})
+export class RestaurantsPage implements OnInit {
+  public note = 9;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
